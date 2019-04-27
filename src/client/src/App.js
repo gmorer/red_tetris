ca6b2d60774277d775af1b02852146aabd870701
gmorer <gmorer@student.42.fr>
2019-04-27 17:40:20 +0200

    delete empty rows

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,68 +1,84 @@
 import React, { useState } from 'react';
 import './App.css';
-import Piece from "./containers/piece"
-import PieceDejaPose from './components/putedPieces'
-import PiecePreview from './components/piecePreview'
+import Piece from './containers/piece';
+import PieceDejaPose from './components/putedPieces';
+import PiecePreview from './components/piecePreview';
 
-import pieces from './ressources/pieces.json'
+import pieces from './ressources/pieces.json';
 
 const CUBE_SIZE = 2;
 const COLUMNS_NUMBER = 10;
 const LIGNE_NUMBER = 20;
 
 const board_style = {
-	backgroundColor: "grey",
+	backgroundColor: 'grey',
 	height: `${CUBE_SIZE * LIGNE_NUMBER}em`,
 	width: `${CUBE_SIZE * COLUMNS_NUMBER}em`,
 	// heigth: "100%",
 	// width: "100%",
-}
+};
 
-const twoDArray = (x, y, fill) => Array(x).fill(null).map(() => Array(y).fill(fill));
+const twoDArray = (x, y, fill) =>
+	Array(x)
+		.fill(null)
+		.map(() => Array(y).fill(fill));
 
 // const isTherePieces = (map) => map.some(columns => columns.some(a => a !== ' '))
 
-const randomPiece = () => pieces[Math.floor(Math.random() * pieces.length)]
+const randomPiece = () => pieces[Math.floor(Math.random() * pieces.length)];
 
-const random_pieces_array = x => Array(x).fill(null).map(_ => Math.floor(Math.random() * pieces.length))
+const random_pieces_array = x =>
+	Array(x)
+		.fill(null)
+		.map(_ => Math.floor(Math.random() * pieces.length));
 
 const pieces_array = random_pieces_array(500);
 
-const delete_completed_row = (tab) => {
-
+const delete_completed_row = tab => {
 	var row_to_delete = [];
 
 	tab.map((columns, i) => {
-		if (!columns.includes(" "))
-			return row_to_delete.push(i)
-		else
-			return null;
+		if (!columns.includes(' ')) return row_to_delete.push(i);
+		else return null;
 	});
 
 	row_to_delete.map(e => {
 		for (var i = 0; i < tab[e].length; i++) {
-			tab[e][i] = " ";
+			tab[e][i] = ' ';
 		}
 	});
-}
+};
 
+const delete_empty_row = tab => {
+	const to_remove = [];
+	tab.forEach((row, index) => {
+		if (!row.some(cube => cube === ' ')) to_remove.push(index);
+	});
+	to_remove.forEach(index => {
+		tab.splice(index, 1);
+		tab.unshift(Array(10).fill(' '));
+	});
+};
 
 const App = () => {
 	// console.log(pieces.length)
 	const [tab, setTab] = useState(twoDArray(20, 10, ' '));
 	const [piece_index, setIndex] = useState(0);
-	const [update, forceUpdate] = useState(Math.random())
+	const [update, forceUpdate] = useState(Math.random());
 	const finish_cb = (pos, piece) => {
 		// console.log(piece)
-		piece.position[pos.rotation].display.forEach((row, y) => row.forEach((cube, x) => {
-			if (cube !== ' ') tab[y + pos.y][x + pos.x] = piece.color;
-		}))
-		setIndex(piece_index + 1)
-		delete_completed_row(tab)
+		piece.position[pos.rotation].display.forEach((row, y) =>
+			row.forEach((cube, x) => {
+				if (cube !== ' ') tab[y + pos.y][x + pos.x] = piece.color;
+			})
+		);
+		setIndex(piece_index + 1);
+		delete_empty_row(tab);
+		// delete_completed_row(tab);
 		//foncton qui check si il y a une
 		setTab(tab);
-		forceUpdate(Math.random())
-	}
+		forceUpdate(Math.random());
+	};
 	return (
 		<div className="App">
 			<div style={board_style}>
@@ -71,6 +87,6 @@ const App = () => {
 			</div>
 			<PiecePreview piece={pieces[pieces_array[piece_index + 1]]} />
 		</div>
-	)
-}
+	);
+};
 export default App;
